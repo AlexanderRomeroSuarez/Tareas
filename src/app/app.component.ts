8d70b8b0eb90bea8b905b8ee248f1515639714d2
Alex Romero <xander_tubb@hotmail.com>
2024-11-30 19:03:22 -0500

    subiendo cambios en index.html y masterfusion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { LoguinComponent } from './loguin/loguin.component';
-import { TareasComponent } from './tareas/tareas.component';
 
 @Component({
   selector: 'app-root',
